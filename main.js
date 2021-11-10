4e9e48f44e59aa4c0357922387e987e47523e529
unknown <shodiyatimilehin@gmail.com>
2021-11-10 22:39:43 +0100

    Big fixes. Comments added

--- main.js
+++ main.js
@@ -38,14 +38,14 @@ class Field {
 };
 
 function startGame(startPos=[0,0], game=new Field(Field.generateField(6,5))) {
-  console.log(game.field);
-  var move = prompt("what direction?");
+  console.log(game.field);                                                              //displays the state of the game
+
+  var move = prompt("what direction?");                                                 //Prompts user input for direction
   if (!['l','r','d','u'].includes(move)) {
-    console.log("You can only move left, right, up or down")
-    return
-  } else {
+    return "You can only move left, right, up or down";                                 //Checks if user input is invalid
+  } else {                                                                              //Exits the function if user input is invalid
     let newPos = startPos;
-    switch(move) {
+    switch(move) {                                                                      //Computes new position
       case "l":
         newPos[1] = newPos[1]-1;
         break;
@@ -57,14 +57,16 @@ function startGame(startPos=[0,0], game=new Field(Field.generateField(6,5))) {
         break;
       case "u":
         newPos[0] = newPos[0]-1;
-    }
-    if (newPos.some(v => v < 0) || newPos[0]>game.field.length || newPos[1]>game.field[0].length) {
+    };
+
+    if (newPos.some(v => v < 0) || newPos[0]==game.field.length || newPos[1]==game.field[0].length) {
         console.log('out');
         return "out of bounds";
     } else if (game.field[newPos[0]][newPos[1]] == "O") {
-        console.log(game.field[newPos[0]][newPos[1]]);
+        console.log(game.field[newPos[0]][newPos[1]])
         return "You fell in a hole";
     } else if (game.field[newPos[0]][newPos[1]] == "^") {
+        console.log("You found it!!!")
         return "You Found It!!!";
     } else {
       game.field[newPos[0]][newPos[1]] = "*";
